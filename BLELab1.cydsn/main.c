b70367240752bc1aec4e7e0ed40bbc9939093c58
emladinov <emladinov01@manhattan.edu>
2016-09-28 19:15:27 -0400

    Make blue LED blink

---
+++ BLELab1.cydsn/main.c
@@ -0,0 +1,24 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include <project.h>
+
+int main()
+{   
+    /* CyGlobalIntEnable; */ /* Uncomment this line to enable global interrupts. */
+    for(;;)
+    {
+        Pin_LED_Write(~Pin_LED_Read());
+        CyDelay(1000);
+    }
+}
+
+/* [] END OF FILE */
